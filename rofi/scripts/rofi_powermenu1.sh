941fc1effd1f8bfc5679ce32e5eabf38a4f8e637
Pranjwal-Jha <pranjwaljha2005@gmail.com>
2026-04-11 23:17:22 +0530

    updates

--- rofi/scripts/rofi_powermenu1.sh
+++ rofi/scripts/rofi_powermenu1.sh
@@ -7,36 +7,16 @@ options="${poweroff_str}\n${reboot_str}\n${logout_str}\n${lock_str}"
 selected_option=$(echo -e "$options" | rofi -dmenu -p "Power" -theme applets/powermenu)
 case "$selected_option" in
     "$poweroff_str")
-        poweroff
+        systemctl poweroff
         ;;
     "$reboot_str")
-        reboot
+        systemctl reboot
         ;;
     "$logout_str")
-        # I saw you are on Hyprland, 'bspc quit' won't work.
-        # Use this instead:
         hyprctl dispatch exit
         ;;
     "$lock_str")
         betterlockscreen --lock dim > /dev/null 2>&1
         ;;
 esac
-# case "$1" in
-#     "poweroff")
-#         poweroff # Assuming 'poweroff' is an executable command
-#         ;;
-#     "reboot")
-#         reboot   # Assuming 'reboot' is an executable command
-#         ;;
-#     "logout")
-#         bspc quit
-#         ;;
-#     "lock")
-#         betterlockscreen --lock dim > /dev/null 2>&1
-#         exit 0
-#         ;;
-#     *)
-#         # Default case: Print the options
-#         echo -e "${poweroff_str}\n${reboot_str}\n${logout_str}\n${lock_str}"
-#         ;;
-# esac
+
